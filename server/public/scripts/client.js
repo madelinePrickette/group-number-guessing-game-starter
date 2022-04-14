897c1c4c5c065e03ccd044b294f08be00c24d046
Madeline Prickette <madelineprickette@gmail.com>
2022-04-14 13:41:47 -0500

    added in objects and creating post function

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -6,8 +6,45 @@ function handleReady() {
   $('#resetGameButton').on('click', onResetClick)
 }
 
+let newGuesses = [];
+
 function onSubmitClick(){
   console.log('submit button works');
+  let chrisGuess = {
+    name: 'Chris',
+    number: $('#chrisInput').val()
+  }
+  let joeGuess = {
+    name: 'Joe',
+    number: $('#joeInput').val()
+  }
+  let madelineGuess = {
+    name: 'Madeline',
+    number: $('#madelineInput').val()
+  }
+  console.log(chrisGuess);
+  console.log(joeGuess);
+  console.log(madelineGuess);
+
+  newGuesses.push(chrisGuess, joeGuess, madelineGuess);
+  postUserGuesses(newGuesses);
+  console.log(newGuesses);
+
+  newGuesses = [];
+
+  console.log(newGuesses);
+}
+
+function postUserGuesses(guesses){
+  $.ajax({
+    method: 'POST',
+    url: '/guessesPage',
+    data: guesses
+  })
+  .then(function (response){
+        //readGuesses();
+  })
+
 }
 
 function onResetClick(){
--- server/server.js
+++ server/server.js
@@ -1,3 +1,5 @@
+const PreviousGuessesList = [];
+
 const express = require('express');
 const bodyParser = require('body-parser')
 const app = express();
