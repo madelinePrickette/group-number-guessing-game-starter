7acf57f6fe4fe622a5411577b9ef3dff52c37132
Madeline Prickette <madelineprickette@gmail.com>
2022-04-14 11:50:05 -0500

    added buttons and input fields and click handlers

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -2,4 +2,14 @@ $(document).ready(handleReady);
 
 function handleReady() {
   console.log("jquery is loaded!")
+  $('#submitButton').on('click', onSubmitClick)
+  $('#resetGameButton').on('click', onResetClick)
+}
+
+function onSubmitClick(){
+  console.log('submit button works');
+}
+
+function onResetClick(){
+  console.log('reset button works');
 }
